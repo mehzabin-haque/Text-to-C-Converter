98de6bf23ff9e8cdca24fba1e76c1bfd2cd7e4b7
mehzabin-haque <74411032+mehzabin-haque@users.noreply.github.com>
2022-02-07 15:36:32 +0600

    Update HelloApplication.java

--- HelloApplication.java
+++ HelloApplication.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+    //kaj kore na why :v
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("../../../../../LogIn/Registration.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 550);
