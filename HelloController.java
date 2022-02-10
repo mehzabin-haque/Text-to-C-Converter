fccd96960dbaa6fb27db694c9d7206cdff73a58b
mehzabin-haque <74411032+mehzabin-haque@users.noreply.github.com>
2022-02-10 16:44:27 +0600

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,48 @@
+package com.example.spl1_t2c;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloController extends SwitchScene{
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public Button signIn;
+    public Button signUp;
+    public Stage stage;
+    public Scene scene;
+    public Parent root;
+
+
+    public void switchToRegister(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("Register.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToLogIn(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("LogIn.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
