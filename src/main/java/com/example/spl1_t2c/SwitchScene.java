2268bd41979bf458260b62d5c048baea194b1ec9
mehzabin-haque <bsse1233@iit.du.ac.bd>
2022-03-02 19:44:12 +0600

    Text to c++ converter

--- src/main/java/com/example/spl1_t2c/SwitchScene.java
+++ src/main/java/com/example/spl1_t2c/SwitchScene.java
@@ -11,24 +11,24 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class SwitchScene {
-        private Stage stage;
-        private Scene scene;
-        private Parent root;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-        public void switchToRegistration(javafx.event.ActionEvent event) throws IOException {
-            root = FXMLLoader.load(getClass().getResource("Register.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        }
+    public void switchToRegistration(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-        public void switchToLogIn(ActionEvent event) throws IOException {
-            Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+    public void switchToLogIn(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void switchToPage1(ActionEvent event) throws IOException {
